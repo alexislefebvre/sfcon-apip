cd1ed30346bd70d0e71570cc7eb4ec268ed1f3b3
Alexis Lefebvre <alexis.lefebvre@les-tilleuls.coop>
2026-01-21 15:52:56 +0100

    chore: search data manually in BookProvider

--- src/State/BookProvider.php
+++ src/State/BookProvider.php
@@ -7,6 +7,7 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Entity\Book;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * @implements ProviderInterface<Book[]|Book|null>
@@ -19,10 +20,50 @@ final readonly class BookProvider implements ProviderInterface
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): iterable|Book|null
     {
+        $apiQueryParameters = $context['request']->attributes->get('_api_query_parameters');
+        $bookRepository = $this->entityManager->getRepository(Book::class);
+
         if ($operation instanceof CollectionOperationInterface) {
-            return $this->entityManager->getRepository(Book::class)->findAll();
+            if ($name = $apiQueryParameters['name'] ?? null) {
+                return $this->findBookByName($name);
+            } elseif ($isbn = $apiQueryParameters['isbn'] ?? null) {
+                return $this->findBookByIsbn($isbn);
+            }
+
+            return $bookRepository->findAll();
         }
 
-        return $this->entityManager->getRepository(Book::class)->find($uriVariables['id']);
+        return $bookRepository->find($uriVariables['id']);
+    }
+
+    private function findBookByName(string $name): iterable
+    {
+        $queryBuilder = $this->getQueryBuilder();
+
+        return $queryBuilder
+            ->where(
+                $queryBuilder->expr()->like('book.title', ':name'),
+            )
+            ->setParameter('name', '%'.$name.'%')
+            ->getQuery()->getResult()
+        ;
+    }
+    private function findBookByIsbn(string $isbn): iterable
+    {
+        $queryBuilder = $this->getQueryBuilder();
+
+        return $queryBuilder
+            ->where(
+                $queryBuilder->expr()->eq('book.isbn', ':isbn'),
+            )
+            ->setParameter('isbn', $isbn)
+            ->getQuery()->getResult()
+        ;
+    }
+
+    private function getQueryBuilder(): QueryBuilder
+    {
+        return $this->entityManager->getRepository(Book::class)
+            ->createQueryBuilder('book');
     }
 }

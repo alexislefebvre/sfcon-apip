a1267f4157420d2563f3f5dc9de822397f652b98
Alexis Lefebvre <alexis.lefebvre@les-tilleuls.coop>
2026-01-21 15:35:24 +0100

    chore: fetch data manually in BookProvider

--- src/State/BookProvider.php
+++ src/State/BookProvider.php
@@ -6,21 +6,23 @@ use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use App\Entity\Book;
+use Doctrine\ORM\EntityManagerInterface;
 
 /**
  * @implements ProviderInterface<Book[]|Book|null>
  */
-class BookProvider implements ProviderInterface
+final readonly class BookProvider implements ProviderInterface
 {
-    // TODO: check if the exception “Typed property App\\State\\BookProvider::$data must not be accessed before initialization” still occur later
-    private array $data = [];
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+    }
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): iterable|Book|null
     {
         if ($operation instanceof CollectionOperationInterface) {
-            return $this->data;
+            return $this->entityManager->getRepository(Book::class)->findAll();
         }
 
-        return $this->data[$uriVariables['id']] ?? null;
+        return $this->entityManager->getRepository(Book::class)->find($uriVariables['id']);
     }
 }
